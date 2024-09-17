f8f09ae3d51d09e8f11edfcf604103714cef9cf4
shaijut <shaiju1319@gmail.com>
2024-09-17 12:48:48 +0530

    Initial commit

---
+++ Controllers/TodoController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TodoAPI.Repositories;
+
+namespace TodoAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TodoController : ControllerBase
+    {
+        private readonly TodoRepository _repository = new TodoRepository();
+
+        [HttpGet]
+        public ActionResult<List<TodoItem>> Get()
+        {
+            return Ok(_repository.GetAll());
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<TodoItem> Get(int id)
+        {
+            var todo = _repository.GetById(id);
+            if (todo == null)
+            {
+                return NotFound();
+            }
+            return Ok(todo);
+        }
+
+        [HttpPost]
+        public ActionResult Post([FromBody] TodoItem todo)
+        {
+            _repository.Add(todo);
+            return CreatedAtAction(nameof(Get), new { id = todo.Id }, todo);
+        }
+
+        [HttpPut("{id}")]
+        public ActionResult Put(int id, [FromBody] TodoItem todo)
+        {
+            var existingTodo = _repository.GetById(id);
+            if (existingTodo == null)
+            {
+                return NotFound();
+            }
+
+            todo.Id = id;
+            _repository.Update(todo);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult Delete(int id)
+        {
+            var existingTodo = _repository.GetById(id);
+            if (existingTodo == null)
+            {
+                return NotFound();
+            }
+
+            _repository.Delete(id);
+            return NoContent();
+        }
+    }
+}
---
+++ Repositories/TodoRepository.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections.Generic;
+using System.Data;
+using Microsoft.Data.Sqlite;
+
+namespace TodoAPI.Repositories
+{
+    public class TodoRepository
+    {
+        private readonly string _connectionString = "Data Source=App_Data/TodoDatabase.db";
+
+        public List<TodoItem> GetAll()
+        {
+            var todos = new List<TodoItem>();
+
+            using (var connection = new SqliteConnection(_connectionString))
+            {
+                connection.Open();
+                var command = connection.CreateCommand();
+                command.CommandText = "SELECT Id, Title, IsCompleted FROM Todos";
+                
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        todos.Add(new TodoItem
+                        {
+                            Id = reader.GetInt32(0),
+                            Title = reader.GetString(1),
+                            IsCompleted = reader.GetBoolean(2)
+                        });
+                    }
+                }
+            }
+
+            return todos;
+        }
+
+        public TodoItem GetById(int id)
+        {
+            using (var connection = new SqliteConnection(_connectionString))
+            {
+                connection.Open();
+                var command = connection.CreateCommand();
+                command.CommandText = "SELECT Id, Title, IsCompleted FROM Todos WHERE Id = @id";
+                command.Parameters.AddWithValue("@id", id);
+
+                using (var reader = command.ExecuteReader())
+                {
+                    if (reader.Read())
+                    {
+                        return new TodoItem
+                        {
+                            Id = reader.GetInt32(0),
+                            Title = reader.GetString(1),
+                            IsCompleted = reader.GetBoolean(2)
+                        };
+                    }
+                }
+            }
+
+            return null;
+        }
+
+        public void Add(TodoItem todo)
+        {
+            using (var connection = new SqliteConnection(_connectionString))
+            {
+                connection.Open();
+                var command = connection.CreateCommand();
+                command.CommandText =
+                @"
+                    INSERT INTO Todos (Title, IsCompleted)
+                    VALUES (@title, @isCompleted);
+                ";
+                command.Parameters.AddWithValue("@title", todo.Title);
+                command.Parameters.AddWithValue("@isCompleted", todo.IsCompleted ? 1 : 0);
+
+                command.ExecuteNonQuery();
+            }
+        }
+
+        public void Update(TodoItem todo)
+        {
+            using (var connection = new SqliteConnection(_connectionString))
+            {
+                connection.Open();
+                var command = connection.CreateCommand();
+                command.CommandText =
+                @"
+                    UPDATE Todos
+                    SET Title = @title, IsCompleted = @isCompleted
+                    WHERE Id = @id;
+                ";
+                command.Parameters.AddWithValue("@id", todo.Id);
+                command.Parameters.AddWithValue("@title", todo.Title);
+                command.Parameters.AddWithValue("@isCompleted", todo.IsCompleted ? 1 : 0);
+
+                command.ExecuteNonQuery();
+            }
+        }
+
+        public void Delete(int id)
+        {
+            using (var connection = new SqliteConnection(_connectionString))
+            {
+                connection.Open();
+                var command = connection.CreateCommand();
+                command.CommandText =
+                @"
+                    DELETE FROM Todos
+                    WHERE Id = @id;
+                ";
+                command.Parameters.AddWithValue("@id", id);
+
+                command.ExecuteNonQuery();
+            }
+        }
+    }
+}

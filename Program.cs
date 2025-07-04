a0cd1f2912bbd5adb470cabcc36d00ffd522f1df
shaijut <29855877+shaijut@users.noreply.github.com>
2025-07-04 23:38:33 +0530

    Upgrade main to net9.0  and Add Open API Doc and Swagger UI Support

--- Program.cs
+++ Program.cs
@@ -14,15 +14,18 @@ namespace TodoAPI
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+            builder.Services.AddOpenApi();
 
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
-                app.UseSwagger();
-                app.UseSwaggerUI();
+                app.MapOpenApi();
+                app.UseSwaggerUI(options =>
+                {
+                    options.SwaggerEndpoint("/openapi/v1.json", "Open API V1");
+                });
 
                 // Automatically open browser at /swagger when using dotnet run while using Kestrel (via CLI)
                 Process.Start(new ProcessStartInfo
